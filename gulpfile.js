6a57150e7f9a3184764818c2f7e96da8897979ef
Yamasheva Olga <olyalya-93@ya.ru>
2019-06-03 15:00:29 +0300

    task727 fines & breaches tablet + gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,70 @@
-const gulp = require('gulp');
-const less = require('gulp-less');
-const concat = require('gulp-concat');
-const minifyCSS = require('gulp-clean-css');
-const sourcemaps = require('gulp-sourcemaps');
-const rename = require('gulp-rename');
-const gulpinclude = require('gulp-file-include');
-const browserSync = require('browser-sync').create();
-var browserSyncReuseTab = require('browser-sync-reuse-tab')(browserSync)
+var gulp = require('gulp'),
+	//watch = require('gulp-watch'),
+	less = require('gulp-less'),
+	concat = require('gulp-concat'),
+	cleanCSS = require('gulp-clean-css'),
+	sourcemaps = require('gulp-sourcemaps'),
+	rename = require('gulp-rename'),
+	gulpinclude = require('gulp-file-include'),
+	browserSync = require('browser-sync');
+//browserSyncReuseTab = require('browser-sync-reuse-tab')(browserSync);
 
-//server
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    port: 3009,
-    server: {
-      baseDir: './build'
-    },
-    open: true // do not automatically open browser
-  }, browserSyncReuseTab);
-  browserSync.watch( 'build', browserSync.reload() );
-});
+const server = browserSync.create();
 
-gulp.task('fileinclude', function() {
-  return gulp.src('./src/*.html')
-    .pipe(gulpinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest('./build/'));
-});
+const path = {
+	build: { //куда складывать готовые после сборки файлы
+		html: 'build/',
+		css: 'build/css/'
+	},
+	src: { //откуда брать исходники
+		html: 'src/*.html',
+		style: 'src/less/styles.less'
+	},
+	watch: { //за изменением каких файлов мы хотим наблюдать
+		html: 'src/**/*.html',
+		style: 'src/less/**/*.less'
+	}
+};
 
-gulp.task('lessPack', async function() {
-  gulp.src(['./src/less/styles.less'])
-    .pipe(sourcemaps.init())
-    .pipe(less())
-    .pipe(concat('main.css'))
-    .pipe(minifyCSS())
-    .pipe(rename({suffix: ".min"}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./build/css'))
-    .pipe(browserSync.reload({stream: true}));
-});
+function serve(done) {
+	server.init({
+		port: 3009,
+		server: {
+			baseDir: './build'
+		},
+		notify: true,
+		open: true
+	});
+	done();
+};
 
-gulp.task('watch', function(cb) {
-  gulp.watch('./src/less/**/*.less', gulp.series('lessPack') );
-  gulp.watch('./src/less/*.less', gulp.series('lessPack') );
-	gulp.watch('./src/**/*.html').on( 'change',gulp.series('fileinclude') );
-	gulp.watch('./build/*.html').on( 'change', browserSync.reload);
-});
+function reload(done) {
+	server.reload();
+	done();
+};
 
-gulp.task('default', gulp.series(
-  gulp.series('fileinclude', 'lessPack'),
-  gulp.parallel('watch', 'browser-sync'),
-));
+function fileinclude() {
+	return gulp.src(path.src.html)
+		.pipe(gulpinclude({
+			prefix: '@@',
+			basepath: '@file'
+		}))
+		.pipe(gulp.dest(path.build.html));
+};
+
+function lessPack() {
+	return gulp.src(path.src.style)
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(concat('main.min.css'))
+		.pipe(cleanCSS())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(path.build.css));
+};
+
+function watch() {
+	gulp.watch(path.watch.html, gulp.series(fileinclude, reload));
+	gulp.watch(path.watch.style, gulp.series(lessPack, reload));
+};
+
+gulp.task('default', gulp.series(fileinclude, lessPack, serve, watch));
