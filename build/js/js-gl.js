64db1680986d0e5c6c0e2f0afcb13319de569684
Ilyklem <ilyklem@gmail.com>
2019-09-04 12:20:05 +0300

    Правка #882.

--- build/js/js-gl.js
+++ build/js/js-gl.js
@@ -58,6 +58,22 @@ function initNavbar() {
     });
 }
 
+(function($) {
+    $.fn.inputFilter = function(inputFilter) {
+        return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            }
+        });
+    };
+}(jQuery));
+
+
 $.widget("custom.hatNavbarContentsSearchField", $.ui.autocomplete, {
     _renderItem: function (ul, item) {
         console.log(item);
