97facf0ffbd5bbbb19f22d3323fb399800a07e0a
anya_romina <anyaromina@mail.ru>
2020-03-04 11:24:33 +0300

    Пересобрано проект

--- new/dist/assets/js/main.js
+++ new/dist/assets/js/main.js
@@ -419,109 +419,6 @@ ko.bindingHandlers.hrmCheckboxGroupLabel = {
 })();
 "use strict";
 
-// hrmDropdownMenu
-(() => {
-  class HrmDropdownMenuViewModel {
-    constructor(element, context, template, placement) {
-      this._subscriptions = [];
-      this._template = template;
-      this._placement = placement;
-      this._context = context;
-      this.element = element;
-      this._tippyInstance = null;
-      this._tooltipClickHandler = null;
-
-      this._init();
-    }
-
-    _init() {
-      let hidingFlag = false;
-
-      this._tooltipClickHandler = event => {
-        const $target = $(event.target);
-
-        if ($target.is('.hrm-dropdown-menu__item:not(.hrm-dropdown-menu__item--disabled)') || $target.parents('.hrm-dropdown-menu__item:not(.hrm-dropdown-menu__item--disabled)').length > 0) {
-          this._tippyInstance.hide();
-        }
-      };
-
-      this._tippyInstance = tippy(this.element, {
-        content: this._createContent(document.getElementById(this._template).innerHTML),
-        arrow: false,
-        distance: 7,
-        interactive: true,
-        placement: this._placement !== undefined ? this._placement : 'bottom',
-        appendTo: document.body,
-        boundary: 'viewport',
-        hideOnClick: true,
-        trigger: 'click',
-        onCreate: instance => {
-          $(instance.popperChildren.tooltip).addClass('hrm-dropdown-menu');
-          $(instance.popperChildren.tooltip).on('click', this._tooltipClickHandler);
-        },
-        onShow: () => {
-          return !hidingFlag;
-        },
-        onMount: instance => {
-          $(instance.popperChildren.content).find('.hrm-dropdown-menu__content').overlayScrollbars({});
-          ko.applyBindingsToDescendants(this._context, instance.popperChildren.content);
-          setTimeout(() => {
-            instance.popperInstance.update();
-          });
-        },
-        onHide: () => {
-          hidingFlag = true;
-        },
-        onHidden: instance => {
-          // Хак, чтобы tippy пересоздал содержимое и можно было применить заново биндинги Knockout
-          instance.setContent(this._createContent(document.getElementById(this._template).innerHTML));
-          hidingFlag = false;
-        }
-      });
-    }
-
-    _destroy() {
-      this._subscriptions.forEach(s => s.dispose());
-
-      this._tippyInstance.destroy();
-    }
-
-    _createContent(template) {
-      return $('<div>').addClass('hrm-dropdown-menu__content').append(template).get()[0];
-    }
-
-  }
-
-  ko.bindingHandlers.hrmDropdownMenu = {
-    init: function (element, valueAccessor, allBindings, _, bindingContext) {
-      const template = allBindings.get('hrmDropdownMenuTemplate');
-      const placement = allBindings.get('hrmDropdownMenuPlacement');
-      const viewModel = new HrmDropdownMenuViewModel(element, bindingContext, template, placement);
-
-      if (valueAccessor() !== undefined) {
-        if (ko.isObservableArray(valueAccessor())) {
-          valueAccessor().push(viewModel);
-        } else {
-          valueAccessor()(viewModel);
-        }
-      }
-
-      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
-        if (valueAccessor() !== undefined) {
-          if (ko.isObservableArray(valueAccessor())) {
-            valueAccessor().remove(this);
-          } else {
-            valueAccessor()(null);
-          }
-        }
-
-        viewModel._destroy();
-      });
-    }
-  };
-})();
-"use strict";
-
 // hrmFormFieldComplexControl
 (() => {
   let nextId = 0;
@@ -1482,6 +1379,109 @@ ko.components.register('hrm-form-field-error', {
 });
 "use strict";
 
+// hrmDropdownMenu
+(() => {
+  class HrmDropdownMenuViewModel {
+    constructor(element, context, template, placement) {
+      this._subscriptions = [];
+      this._template = template;
+      this._placement = placement;
+      this._context = context;
+      this.element = element;
+      this._tippyInstance = null;
+      this._tooltipClickHandler = null;
+
+      this._init();
+    }
+
+    _init() {
+      let hidingFlag = false;
+
+      this._tooltipClickHandler = event => {
+        const $target = $(event.target);
+
+        if ($target.is('.hrm-dropdown-menu__item:not(.hrm-dropdown-menu__item--disabled)') || $target.parents('.hrm-dropdown-menu__item:not(.hrm-dropdown-menu__item--disabled)').length > 0) {
+          this._tippyInstance.hide();
+        }
+      };
+
+      this._tippyInstance = tippy(this.element, {
+        content: this._createContent(document.getElementById(this._template).innerHTML),
+        arrow: false,
+        distance: 7,
+        interactive: true,
+        placement: this._placement !== undefined ? this._placement : 'bottom',
+        appendTo: document.body,
+        boundary: 'viewport',
+        hideOnClick: true,
+        trigger: 'click',
+        onCreate: instance => {
+          $(instance.popperChildren.tooltip).addClass('hrm-dropdown-menu');
+          $(instance.popperChildren.tooltip).on('click', this._tooltipClickHandler);
+        },
+        onShow: () => {
+          return !hidingFlag;
+        },
+        onMount: instance => {
+          $(instance.popperChildren.content).find('.hrm-dropdown-menu__content').overlayScrollbars({});
+          ko.applyBindingsToDescendants(this._context, instance.popperChildren.content);
+          setTimeout(() => {
+            instance.popperInstance.update();
+          });
+        },
+        onHide: () => {
+          hidingFlag = true;
+        },
+        onHidden: instance => {
+          // Хак, чтобы tippy пересоздал содержимое и можно было применить заново биндинги Knockout
+          instance.setContent(this._createContent(document.getElementById(this._template).innerHTML));
+          hidingFlag = false;
+        }
+      });
+    }
+
+    _destroy() {
+      this._subscriptions.forEach(s => s.dispose());
+
+      this._tippyInstance.destroy();
+    }
+
+    _createContent(template) {
+      return $('<div>').addClass('hrm-dropdown-menu__content').append(template).get()[0];
+    }
+
+  }
+
+  ko.bindingHandlers.hrmDropdownMenu = {
+    init: function (element, valueAccessor, allBindings, _, bindingContext) {
+      const template = allBindings.get('hrmDropdownMenuTemplate');
+      const placement = allBindings.get('hrmDropdownMenuPlacement');
+      const viewModel = new HrmDropdownMenuViewModel(element, bindingContext, template, placement);
+
+      if (valueAccessor() !== undefined) {
+        if (ko.isObservableArray(valueAccessor())) {
+          valueAccessor().push(viewModel);
+        } else {
+          valueAccessor()(viewModel);
+        }
+      }
+
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+        if (valueAccessor() !== undefined) {
+          if (ko.isObservableArray(valueAccessor())) {
+            valueAccessor().remove(this);
+          } else {
+            valueAccessor()(null);
+          }
+        }
+
+        viewModel._destroy();
+      });
+    }
+  };
+})();
+"use strict";
+
 (() => {
   ko.components.register('hrm-main-sidebar', {
     viewModel: {
@@ -2041,120 +2041,6 @@ ko.components.register('hrm-form-field-error', {
 })();
 "use strict";
 
-// hrmScrollable
-(() => {
-  class HrmScrollableViewModel {
-    constructor(element, disabled) {
-      this._subscriptions = [];
-      this._disabledSubscription = null;
-      this._disabled = null;
-      this._overlayScrollbarsInstance = null;
-      this._overlayScrollbarsOptions = {
-        className: 'hrm-scrollable'
-      };
-      this.element = element;
-
-      this._init(disabled);
-    }
-
-    _init(disabled) {
-      this._setDisabled(disabled);
-
-      this._update();
-    }
-
-    _destroy() {
-      this._subscriptions.forEach(s => s.dispose());
-
-      this._disabledSubscription.dispose();
-    }
-
-    _setDisabled(disabled) {
-      if (disabled === undefined) {
-        disabled = false;
-      }
-
-      if (this._disabledSubscription !== null) {
-        this._disabledSubscription.dispose();
-      }
-
-      if (ko.isObservable(disabled)) {
-        this._disabledSubscription = disabled.subscribe(disabled => {
-          this._disabled = disabled;
-
-          this._update();
-        });
-        this._disabled = disabled();
-      } else {
-        this._disabled = disabled;
-
-        this._update();
-      }
-    }
-
-    _update() {
-      const $element = $(this.element);
-
-      if (!this._disabled) {
-        if (this._overlayScrollbarsInstance === null) {
-          $element.overlayScrollbars(this._overlayScrollbarsOptions);
-          this._overlayScrollbarsInstance = $element.overlayScrollbars();
-        }
-      } else {
-        if (this._overlayScrollbarsInstance !== null) {
-          this._overlayScrollbarsInstance.destroy();
-
-          this._overlayScrollbarsInstance = null;
-        }
-      }
-    }
-
-  }
-
-  const instances = new Map();
-  const previousBindingsList = new Map();
-  ko.bindingHandlers.hrmScrollable = {
-    init: function (element, valueAccessor, allBindings) {
-      const disabled = allBindings.get('hrmScrollableDisabled');
-      const viewModel = new HrmScrollableViewModel(element, disabled);
-      instances.set(element, viewModel);
-
-      if (valueAccessor() !== undefined) {
-        if (ko.isObservableArray(valueAccessor())) {
-          valueAccessor().push(viewModel);
-        } else {
-          valueAccessor()(viewModel);
-        }
-      }
-
-      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
-        if (valueAccessor() !== undefined) {
-          if (ko.isObservableArray(valueAccessor())) {
-            valueAccessor().remove(this);
-          } else {
-            valueAccessor()(null);
-          }
-        }
-
-        viewModel._destroy();
-      });
-    },
-    update: function (element, valueAccessor, allBindings) {
-      const instance = instances.get(element);
-      const previousBindings = previousBindingsList.get(element);
-
-      if (previousBindings !== undefined) {
-        if (previousBindings['hrmScrollableDisabled'] !== allBindings.get('hrmScrollableDisabled')) {
-          instance._setDisabled(allBindings.get('hrmScrollableDisabled'));
-        }
-      }
-
-      previousBindingsList.set(element, allBindings());
-    }
-  };
-})();
-"use strict";
-
 ko.bindingHandlers.hrmSelect = {
   init: function (element, valueAccessor, allBindings) {
     const $element = $(element);
@@ -2270,6 +2156,120 @@ ko.bindingHandlers.hrmSelect = {
     }
   }
 };
+"use strict";
+
+// hrmScrollable
+(() => {
+  class HrmScrollableViewModel {
+    constructor(element, disabled) {
+      this._subscriptions = [];
+      this._disabledSubscription = null;
+      this._disabled = null;
+      this._overlayScrollbarsInstance = null;
+      this._overlayScrollbarsOptions = {
+        className: 'hrm-scrollable'
+      };
+      this.element = element;
+
+      this._init(disabled);
+    }
+
+    _init(disabled) {
+      this._setDisabled(disabled);
+
+      this._update();
+    }
+
+    _destroy() {
+      this._subscriptions.forEach(s => s.dispose());
+
+      this._disabledSubscription.dispose();
+    }
+
+    _setDisabled(disabled) {
+      if (disabled === undefined) {
+        disabled = false;
+      }
+
+      if (this._disabledSubscription !== null) {
+        this._disabledSubscription.dispose();
+      }
+
+      if (ko.isObservable(disabled)) {
+        this._disabledSubscription = disabled.subscribe(disabled => {
+          this._disabled = disabled;
+
+          this._update();
+        });
+        this._disabled = disabled();
+      } else {
+        this._disabled = disabled;
+
+        this._update();
+      }
+    }
+
+    _update() {
+      const $element = $(this.element);
+
+      if (!this._disabled) {
+        if (this._overlayScrollbarsInstance === null) {
+          $element.overlayScrollbars(this._overlayScrollbarsOptions);
+          this._overlayScrollbarsInstance = $element.overlayScrollbars();
+        }
+      } else {
+        if (this._overlayScrollbarsInstance !== null) {
+          this._overlayScrollbarsInstance.destroy();
+
+          this._overlayScrollbarsInstance = null;
+        }
+      }
+    }
+
+  }
+
+  const instances = new Map();
+  const previousBindingsList = new Map();
+  ko.bindingHandlers.hrmScrollable = {
+    init: function (element, valueAccessor, allBindings) {
+      const disabled = allBindings.get('hrmScrollableDisabled');
+      const viewModel = new HrmScrollableViewModel(element, disabled);
+      instances.set(element, viewModel);
+
+      if (valueAccessor() !== undefined) {
+        if (ko.isObservableArray(valueAccessor())) {
+          valueAccessor().push(viewModel);
+        } else {
+          valueAccessor()(viewModel);
+        }
+      }
+
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+        if (valueAccessor() !== undefined) {
+          if (ko.isObservableArray(valueAccessor())) {
+            valueAccessor().remove(this);
+          } else {
+            valueAccessor()(null);
+          }
+        }
+
+        viewModel._destroy();
+      });
+    },
+    update: function (element, valueAccessor, allBindings) {
+      const instance = instances.get(element);
+      const previousBindings = previousBindingsList.get(element);
+
+      if (previousBindings !== undefined) {
+        if (previousBindings['hrmScrollableDisabled'] !== allBindings.get('hrmScrollableDisabled')) {
+          instance._setDisabled(allBindings.get('hrmScrollableDisabled'));
+        }
+      }
+
+      previousBindingsList.set(element, allBindings());
+    }
+  };
+})();
 "use strict";
 
 // hrmSwitch
