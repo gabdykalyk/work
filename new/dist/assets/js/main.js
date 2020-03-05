848fc30825be05d9e87f4178ae8035a8d27cff24
Ilyklem <ilyklem@gmail.com>
2020-03-05 14:37:46 +0300

    Правки #1131.

--- new/dist/assets/js/main.js
+++ new/dist/assets/js/main.js
@@ -175,23 +175,6 @@ function hrmExtractComponentParam(params, name, defaultValue) {
 }
 "use strict";
 
-ko.components.register('hrm-basic-sidebar', {
-  viewModel: {
-    createViewModel: function (params, componentInfo) {
-      const $element = $(componentInfo.element);
-      $element.addClass(['hrm-basic-sidebar']);
-      return new function () {}();
-    }
-  },
-  template: `
-        <button class="hrm-button hrm-circle-icon-button hrm-circle-logout-icon-button hrm-basic-sidebar__logout-button"
-                title="Войти/зарегистрироваться">
-        </button>
-        <a class="hrm-basic-sidebar__support-link" href="#" title="Помощь"></a>
-    `
-});
-"use strict";
-
 ko.components.register('hrm-basic-footer', {
   viewModel: {
     createViewModel: function (params, componentInfo) {
@@ -206,6 +189,23 @@ ko.components.register('hrm-basic-footer', {
             <span class="hrm-basic-footer__copyright">© Lookin, 2020</span>
         </div>
         <a class="hrm-basic-footer__support-link" href="#">Техническая поддержка</a>
+    `
+});
+"use strict";
+
+ko.components.register('hrm-basic-sidebar', {
+  viewModel: {
+    createViewModel: function (params, componentInfo) {
+      const $element = $(componentInfo.element);
+      $element.addClass(['hrm-basic-sidebar']);
+      return new function () {}();
+    }
+  },
+  template: `
+        <button class="hrm-button hrm-circle-icon-button hrm-circle-logout-icon-button hrm-basic-sidebar__logout-button"
+                title="Войти/зарегистрироваться">
+        </button>
+        <a class="hrm-basic-sidebar__support-link" href="#" title="Помощь"></a>
     `
 });
 "use strict";
@@ -2038,6 +2038,120 @@ ko.components.register('hrm-form-field-error', {
 })();
 "use strict";
 
+// hrmScrollable
+(() => {
+  class HrmScrollableViewModel {
+    constructor(element, disabled) {
+      this._subscriptions = [];
+      this._disabledSubscription = null;
+      this._disabled = null;
+      this._overlayScrollbarsInstance = null;
+      this._overlayScrollbarsOptions = {
+        className: 'hrm-scrollable'
+      };
+      this.element = element;
+
+      this._init(disabled);
+    }
+
+    _init(disabled) {
+      this._setDisabled(disabled);
+
+      this._update();
+    }
+
+    _destroy() {
+      this._subscriptions.forEach(s => s.dispose());
+
+      this._disabledSubscription.dispose();
+    }
+
+    _setDisabled(disabled) {
+      if (disabled === undefined) {
+        disabled = false;
+      }
+
+      if (this._disabledSubscription !== null) {
+        this._disabledSubscription.dispose();
+      }
+
+      if (ko.isObservable(disabled)) {
+        this._disabledSubscription = disabled.subscribe(disabled => {
+          this._disabled = disabled;
+
+          this._update();
+        });
+        this._disabled = disabled();
+      } else {
+        this._disabled = disabled;
+
+        this._update();
+      }
+    }
+
+    _update() {
+      const $element = $(this.element);
+
+      if (!this._disabled) {
+        if (this._overlayScrollbarsInstance === null) {
+          $element.overlayScrollbars(this._overlayScrollbarsOptions);
+          this._overlayScrollbarsInstance = $element.overlayScrollbars();
+        }
+      } else {
+        if (this._overlayScrollbarsInstance !== null) {
+          this._overlayScrollbarsInstance.destroy();
+
+          this._overlayScrollbarsInstance = null;
+        }
+      }
+    }
+
+  }
+
+  const instances = new Map();
+  const previousBindingsList = new Map();
+  ko.bindingHandlers.hrmScrollable = {
+    init: function (element, valueAccessor, allBindings) {
+      const disabled = allBindings.get('hrmScrollableDisabled');
+      const viewModel = new HrmScrollableViewModel(element, disabled);
+      instances.set(element, viewModel);
+
+      if (valueAccessor() !== undefined) {
+        if (ko.isObservableArray(valueAccessor())) {
+          valueAccessor().push(viewModel);
+        } else {
+          valueAccessor()(viewModel);
+        }
+      }
+
+      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+        if (valueAccessor() !== undefined) {
+          if (ko.isObservableArray(valueAccessor())) {
+            valueAccessor().remove(this);
+          } else {
+            valueAccessor()(null);
+          }
+        }
+
+        viewModel._destroy();
+      });
+    },
+    update: function (element, valueAccessor, allBindings) {
+      const instance = instances.get(element);
+      const previousBindings = previousBindingsList.get(element);
+
+      if (previousBindings !== undefined) {
+        if (previousBindings['hrmScrollableDisabled'] !== allBindings.get('hrmScrollableDisabled')) {
+          instance._setDisabled(allBindings.get('hrmScrollableDisabled'));
+        }
+      }
+
+      previousBindingsList.set(element, allBindings());
+    }
+  };
+})();
+"use strict";
+
 ko.bindingHandlers.hrmSelect = {
   init: function (element, valueAccessor, allBindings) {
     const $element = $(element);
@@ -2153,120 +2267,6 @@ ko.bindingHandlers.hrmSelect = {
     }
   }
 };
-"use strict";
-
-// hrmScrollable
-(() => {
-  class HrmScrollableViewModel {
-    constructor(element, disabled) {
-      this._subscriptions = [];
-      this._disabledSubscription = null;
-      this._disabled = null;
-      this._overlayScrollbarsInstance = null;
-      this._overlayScrollbarsOptions = {
-        className: 'hrm-scrollable'
-      };
-      this.element = element;
-
-      this._init(disabled);
-    }
-
-    _init(disabled) {
-      this._setDisabled(disabled);
-
-      this._update();
-    }
-
-    _destroy() {
-      this._subscriptions.forEach(s => s.dispose());
-
-      this._disabledSubscription.dispose();
-    }
-
-    _setDisabled(disabled) {
-      if (disabled === undefined) {
-        disabled = false;
-      }
-
-      if (this._disabledSubscription !== null) {
-        this._disabledSubscription.dispose();
-      }
-
-      if (ko.isObservable(disabled)) {
-        this._disabledSubscription = disabled.subscribe(disabled => {
-          this._disabled = disabled;
-
-          this._update();
-        });
-        this._disabled = disabled();
-      } else {
-        this._disabled = disabled;
-
-        this._update();
-      }
-    }
-
-    _update() {
-      const $element = $(this.element);
-
-      if (!this._disabled) {
-        if (this._overlayScrollbarsInstance === null) {
-          $element.overlayScrollbars(this._overlayScrollbarsOptions);
-          this._overlayScrollbarsInstance = $element.overlayScrollbars();
-        }
-      } else {
-        if (this._overlayScrollbarsInstance !== null) {
-          this._overlayScrollbarsInstance.destroy();
-
-          this._overlayScrollbarsInstance = null;
-        }
-      }
-    }
-
-  }
-
-  const instances = new Map();
-  const previousBindingsList = new Map();
-  ko.bindingHandlers.hrmScrollable = {
-    init: function (element, valueAccessor, allBindings) {
-      const disabled = allBindings.get('hrmScrollableDisabled');
-      const viewModel = new HrmScrollableViewModel(element, disabled);
-      instances.set(element, viewModel);
-
-      if (valueAccessor() !== undefined) {
-        if (ko.isObservableArray(valueAccessor())) {
-          valueAccessor().push(viewModel);
-        } else {
-          valueAccessor()(viewModel);
-        }
-      }
-
-      ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
-        if (valueAccessor() !== undefined) {
-          if (ko.isObservableArray(valueAccessor())) {
-            valueAccessor().remove(this);
-          } else {
-            valueAccessor()(null);
-          }
-        }
-
-        viewModel._destroy();
-      });
-    },
-    update: function (element, valueAccessor, allBindings) {
-      const instance = instances.get(element);
-      const previousBindings = previousBindingsList.get(element);
-
-      if (previousBindings !== undefined) {
-        if (previousBindings['hrmScrollableDisabled'] !== allBindings.get('hrmScrollableDisabled')) {
-          instance._setDisabled(allBindings.get('hrmScrollableDisabled'));
-        }
-      }
-
-      previousBindingsList.set(element, allBindings());
-    }
-  };
-})();
 "use strict";
 
 // hrmSwitch
