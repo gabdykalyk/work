b2ab8adaf44f15acc73916d81b34ba49e5cacc92
anya_romina <anyaromina@mail.ru>
2020-03-19 11:47:05 +0300

    Пересобран проект

--- new/dist/assets/js/main.js
+++ new/dist/assets/js/main.js
@@ -214,23 +214,6 @@ ko.components.register('hrm-basic-footer', {
 });
 "use strict";
 
-ko.components.register('hrm-basic-sidebar', {
-  viewModel: {
-    createViewModel: function (params, componentInfo) {
-      const $element = $(componentInfo.element);
-      $element.addClass(['hrm-basic-sidebar']);
-      return new function () {}();
-    }
-  },
-  template: `
-        <button class="hrm-button hrm-circle-icon-button hrm-circle-logout-icon-button hrm-basic-sidebar__logout-button"
-                title="Войти/зарегистрироваться">
-        </button>
-        <a class="hrm-basic-sidebar__support-link" href="#" title="Помощь"></a>
-    `
-});
-"use strict";
-
 ko.components.register('hrm-checkbox', {
   viewModel: {
     createViewModel: function (params, componentInfo) {
@@ -317,6 +300,23 @@ ko.bindingHandlers.hrmCheckboxGroupLabel = {
     $element.attr('for', checkboxGroup().id);
   }
 };
+"use strict";
+
+ko.components.register('hrm-basic-sidebar', {
+  viewModel: {
+    createViewModel: function (params, componentInfo) {
+      const $element = $(componentInfo.element);
+      $element.addClass(['hrm-basic-sidebar']);
+      return new function () {}();
+    }
+  },
+  template: `
+        <button class="hrm-button hrm-circle-icon-button hrm-circle-logout-icon-button hrm-basic-sidebar__logout-button"
+                title="Войти/зарегистрироваться">
+        </button>
+        <a class="hrm-basic-sidebar__support-link" href="#" title="Помощь"></a>
+    `
+});
 "use strict";
 
 (() => {
