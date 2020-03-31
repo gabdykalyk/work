f044bdadd1bd7c26ea6fa1274c06d0906ce10fef
Ilyklem <ilyklem@gmail.com>
2020-03-31 12:35:08 +0300

    Правки нового дизайна.

--- new/dist/assets/js/main.js
+++ new/dist/assets/js/main.js
@@ -1521,102 +1521,6 @@ ko.components.register('hrm-form-field-error', {
 });
 "use strict";
 
-ko.bindingHandlers.hrmModalContainerModalWrapper = {
-  init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-    const options = $.extend({
-      closeOnBackdropClick: true,
-      escapePress: 'close'
-    }, allBindings()['hrmModalContainerModalWrapperOptions']);
-    const close = allBindings()['hrmModalContainerModalWrapperClose'];
-    const $element = $(element);
-    const $modal = $element.find('.hrm-modal');
-    $modal.appendTo('body');
-    $modal.modal({
-      backdrop: false,
-      keyboard: false,
-      focus: true
-    });
-
-    if ('modalClass' in options) {
-      $modal.addClass(options.modalClass);
-    }
-
-    $modal.keyup(event => {
-      if (event.keyCode === 27) {
-        if (options.escapePress === 'close') {
-          close.call(viewModel);
-        } else if (options.escapePress instanceof Function) {
-          options.escapePress();
-        }
-      }
-    });
-
-    if (options.closeOnBackdropClick) {
-      $modal.on('click', event => {
-        if (event.target === $modal.get()[0]) {
-          close.call(viewModel);
-        }
-      });
-    }
-
-    const innerBindingContext = ko.bindingEvent.startPossiblyAsyncContentBinding($modal.get()[0], bindingContext);
-    ko.applyBindings(innerBindingContext, $modal.get()[0]);
-    ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
-      $modal.modal('hide').remove();
-    });
-  }
-};
-ko.components.register('hrm-modal-container', {
-  viewModel: {
-    createViewModel: function (params) {
-      const viewModel = new function () {
-        this.opens = params.opens;
-
-        this.close = function (open, value) {
-          this.opens.remove(open);
-
-          if ('close' in open) {
-            open.close(value);
-          }
-        };
-      }();
-      viewModel.initializing = ko.observable(true);
-
-      viewModel.onInit = function () {
-        viewModel.initializing(false);
-      };
-
-      return viewModel;
-    }
-  },
-  template: `
-        <!-- ko template: {afterRender: onInit} -->
-            <!-- ko foreach: opens -->
-                <!-- ko let: {modalElement: ko.observable()} -->
-                    <div data-bind="
-                         hrmModalContainerModalWrapper,
-                         hrmModalContainerModalWrapperOptions: $data.options,
-                         hrmModalContainerModalWrapperClose: function (value) {$component.close($data, value);}
-                    ">
-                        <div class="modal hrm-modal" role="dialog" tabindex="1" data-bind="element: modalElement">
-                            <!-- ko template: {
-                                name: dialogTemplateName,
-                                data: {
-                                    data: $data.data,
-                                    modalElement: modalElement(),
-                                    close: function (value) {$component.close($data, value);}
-                                }
-                            } -->
-                            <!-- /ko -->
-                        </div>
-                    </div>
-                <!-- /ko -->
-            <!-- /ko -->
-        <!-- /ko -->
-    `
-});
-"use strict";
-
 (() => {
   ko.components.register('hrm-main-sidebar', {
     viewModel: {
@@ -2176,6 +2080,102 @@ ko.components.register('hrm-modal-container', {
 })();
 "use strict";
 
+ko.bindingHandlers.hrmModalContainerModalWrapper = {
+  init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+    const options = $.extend({
+      closeOnBackdropClick: true,
+      escapePress: 'close'
+    }, allBindings()['hrmModalContainerModalWrapperOptions']);
+    const close = allBindings()['hrmModalContainerModalWrapperClose'];
+    const $element = $(element);
+    const $modal = $element.find('.hrm-modal');
+    $modal.appendTo('body');
+    $modal.modal({
+      backdrop: false,
+      keyboard: false,
+      focus: true
+    });
+
+    if ('modalClass' in options) {
+      $modal.addClass(options.modalClass);
+    }
+
+    $modal.keyup(event => {
+      if (event.keyCode === 27) {
+        if (options.escapePress === 'close') {
+          close.call(viewModel);
+        } else if (options.escapePress instanceof Function) {
+          options.escapePress();
+        }
+      }
+    });
+
+    if (options.closeOnBackdropClick) {
+      $modal.on('click', event => {
+        if (event.target === $modal.get()[0]) {
+          close.call(viewModel);
+        }
+      });
+    }
+
+    const innerBindingContext = ko.bindingEvent.startPossiblyAsyncContentBinding($modal.get()[0], bindingContext);
+    ko.applyBindings(innerBindingContext, $modal.get()[0]);
+    ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+      $modal.modal('hide').remove();
+    });
+  }
+};
+ko.components.register('hrm-modal-container', {
+  viewModel: {
+    createViewModel: function (params) {
+      const viewModel = new function () {
+        this.opens = params.opens;
+
+        this.close = function (open, value) {
+          this.opens.remove(open);
+
+          if ('close' in open) {
+            open.close(value);
+          }
+        };
+      }();
+      viewModel.initializing = ko.observable(true);
+
+      viewModel.onInit = function () {
+        viewModel.initializing(false);
+      };
+
+      return viewModel;
+    }
+  },
+  template: `
+        <!-- ko template: {afterRender: onInit} -->
+            <!-- ko foreach: opens -->
+                <!-- ko let: {modalElement: ko.observable()} -->
+                    <div data-bind="
+                         hrmModalContainerModalWrapper,
+                         hrmModalContainerModalWrapperOptions: $data.options,
+                         hrmModalContainerModalWrapperClose: function (value) {$component.close($data, value);}
+                    ">
+                        <div class="modal hrm-modal" role="dialog" tabindex="1" data-bind="element: modalElement">
+                            <!-- ko template: {
+                                name: dialogTemplateName,
+                                data: {
+                                    data: $data.data,
+                                    modalElement: modalElement(),
+                                    close: function (value) {$component.close($data, value);}
+                                }
+                            } -->
+                            <!-- /ko -->
+                        </div>
+                    </div>
+                <!-- /ko -->
+            <!-- /ko -->
+        <!-- /ko -->
+    `
+});
+"use strict";
+
 // hrmScrollable
 (() => {
   class HrmScrollableViewModel {
@@ -2562,154 +2562,6 @@ ko.bindingHandlers.hrmSelect = {
 };
 "use strict";
 
-class HrmTabGroupItem {
-  constructor(text, disabled = false) {
-    this.text = text;
-    this.disabled = ko.observable(disabled);
-  }
-
-}
-
-ko.components.register('hrm-tab-group', {
-  viewModel: {
-    createViewModel: function (params, componentInfo) {
-      const $element = $(componentInfo.element);
-      $element.addClass(['hrm-tab-group']);
-
-      const HrmTabGroupViewModel = function () {
-        this.moreButtonElementWidth = 33.5;
-        this.itemMargin = 30;
-        this.subscriptions = [];
-        this.element = componentInfo.element;
-
-        this.windowResizeHandler = () => {
-          this.viewportSize({
-            width: $(window).width(),
-            height: $(window).height()
-          });
-          this.updateWidth();
-        };
-
-        this.viewportSize = ko.observable({
-          width: $(window).width(),
-          height: $(window).height()
-        });
-        this.items = params.items;
-        this.activeItem = hrmExtractComponentParam(params, 'activeItem', 0);
-        this.width = ko.observable($element.width());
-        this.itemWidths = ko.pureComputed(() => {
-          const $itemMirror = $('<span>').css({
-            'font-weight': 500,
-            'font-size': '13px',
-            'white-space': 'nowrap'
-          });
-          const $container = $('<div>').css({
-            position: 'absolute',
-            left: '0',
-            top: '0',
-            width: '100%',
-            height: '100%',
-            visibility: 'hidden'
-          });
-          $container.appendTo(document.body);
-          $container.append($itemMirror);
-          const result = ko.unwrap(this.items).map(item => {
-            $itemMirror.text(item.text);
-            return $itemMirror.width();
-          });
-          $container.remove();
-          return result;
-        });
-        this.maxFitItem = ko.pureComputed(() => {
-          const itemWidths = this.itemWidths();
-          const width = this.width();
-          let w = 0;
-
-          for (let i = 0; i < itemWidths.length; i++) {
-            if (i > 0) {
-              w += itemWidths[i] + this.itemMargin;
-            } else {
-              w += itemWidths[i];
-            }
-
-            if (w >= width - this.moreButtonElementWidth - this.itemMargin) {
-              return i !== 0 ? i - 1 : null;
-            }
-          }
-
-          return itemWidths.length > 0 ? itemWidths.length - 1 : null;
-        });
-
-        this.updateWidth = function () {
-          this.width($element.width());
-        };
-
-        this.timer = ko.observable(0);
-        setInterval(() => {
-          this.timer(this.timer() + 1);
-        }, 1000);
-
-        (() => {
-          $(window).on('resize', this.windowResizeHandler);
-        })();
-      };
-
-      HrmTabGroupViewModel.prototype.dispose = function () {
-        this.subscriptions.forEach(s => s.dispose());
-        $(window).off('resize', this.windowResizeHandler);
-      };
-
-      return new HrmTabGroupViewModel();
-    }
-  },
-  template: `
-        <div class="hrm-tab-group__content-wrapper">
-            <div class="hrm-tab-group__content">
-                <!-- ko if: items.length > 0 -->
-                    <!-- ko foreach: viewportSize().width > HRM_BREAKPOINTS.tabletMaxWidth ? items.slice(0, maxFitItem() + 1) : items -->
-                        <div class="hrm-tab-group__item"
-                             data-bind="
-                                click: function() {if (!disabled()) {$component.activeItem($index());}},
-                                css: {
-                                    'hrm-tab-group__item--active': $component.activeItem() === $index(),
-                                    'hrm-tab-group__item--disabled': disabled
-                                },
-                                text: text
-                             ">
-                        </div>
-                    <!-- /ko -->
-                    
-                    <!-- ko if: viewportSize().width > HRM_BREAKPOINTS.tabletMaxWidth -->
-                        <!-- ko if: maxFitItem() < items.length - 1 -->
-                            <button class="hrm-button hrm-tab-group__more-button"
-                                    data-bind="
-                                        hrmDropdownMenu,
-                                        hrmDropdownMenuTemplate: 'hrm-tab-group-more-button-dropdown-menu-template',
-                                        hrmDropdownMenuPlacement: 'bottom-end'
-                                    ">
-                                Еще...
-                            </button>
-                            
-                            <script id="hrm-tab-group-more-button-dropdown-menu-template" type="text/html">
-                                <!-- ko foreach: items.slice(maxFitItem() + 1) -->
-                                    <div class="hrm-dropdown-menu__item"
-                                         data-bind="
-                                            text: text,
-                                            click: function() {if (!disabled()) {$component.activeItem($index() + $component.maxFitItem() + 1);}},
-                                            css: {'hrm-dropdown-menu__item--disabled': disabled}
-                                         ">
-                                    </div>
-                                <!-- /ko -->
-                            </script>   
-                        <!-- /ko -->
-                    <!-- /ko -->
-                <!-- /ko -->
-            </div>
-        </div>
-    `
-});
-"use strict";
-
 // hrmSwitch
 (() => {
   let nextId = 0;
@@ -2859,6 +2711,154 @@ ko.components.register('hrm-tab-group', {
     }
   };
 })();
+"use strict";
+
+class HrmTabGroupItem {
+  constructor(text, disabled = false) {
+    this.text = text;
+    this.disabled = ko.observable(disabled);
+  }
+
+}
+
+ko.components.register('hrm-tab-group', {
+  viewModel: {
+    createViewModel: function (params, componentInfo) {
+      const $element = $(componentInfo.element);
+      $element.addClass(['hrm-tab-group']);
+
+      const HrmTabGroupViewModel = function () {
+        this.moreButtonElementWidth = 33.5;
+        this.itemMargin = 30;
+        this.subscriptions = [];
+        this.element = componentInfo.element;
+
+        this.windowResizeHandler = () => {
+          this.viewportSize({
+            width: $(window).width(),
+            height: $(window).height()
+          });
+          this.updateWidth();
+        };
+
+        this.viewportSize = ko.observable({
+          width: $(window).width(),
+          height: $(window).height()
+        });
+        this.items = params.items;
+        this.activeItem = hrmExtractComponentParam(params, 'activeItem', 0);
+        this.width = ko.observable($element.width());
+        this.itemWidths = ko.pureComputed(() => {
+          const $itemMirror = $('<span>').css({
+            'font-weight': 500,
+            'font-size': '13px',
+            'white-space': 'nowrap'
+          });
+          const $container = $('<div>').css({
+            position: 'absolute',
+            left: '0',
+            top: '0',
+            width: '100%',
+            height: '100%',
+            visibility: 'hidden'
+          });
+          $container.appendTo(document.body);
+          $container.append($itemMirror);
+          const result = ko.unwrap(this.items).map(item => {
+            $itemMirror.text(item.text);
+            return $itemMirror.width();
+          });
+          $container.remove();
+          return result;
+        });
+        this.maxFitItem = ko.pureComputed(() => {
+          const itemWidths = this.itemWidths();
+          const width = this.width();
+          let w = 0;
+
+          for (let i = 0; i < itemWidths.length; i++) {
+            if (i > 0) {
+              w += itemWidths[i] + this.itemMargin;
+            } else {
+              w += itemWidths[i];
+            }
+
+            if (w >= width - this.moreButtonElementWidth - this.itemMargin) {
+              return i !== 0 ? i - 1 : null;
+            }
+          }
+
+          return itemWidths.length > 0 ? itemWidths.length - 1 : null;
+        });
+
+        this.updateWidth = function () {
+          this.width($element.width());
+        };
+
+        this.timer = ko.observable(0);
+        setInterval(() => {
+          this.timer(this.timer() + 1);
+        }, 1000);
+
+        (() => {
+          $(window).on('resize', this.windowResizeHandler);
+        })();
+      };
+
+      HrmTabGroupViewModel.prototype.dispose = function () {
+        this.subscriptions.forEach(s => s.dispose());
+        $(window).off('resize', this.windowResizeHandler);
+      };
+
+      return new HrmTabGroupViewModel();
+    }
+  },
+  template: `
+        <div class="hrm-tab-group__content-wrapper">
+            <div class="hrm-tab-group__content">
+                <!-- ko if: items.length > 0 -->
+                    <!-- ko foreach: viewportSize().width > HRM_BREAKPOINTS.tabletMaxWidth ? items.slice(0, maxFitItem() + 1) : items -->
+                        <div class="hrm-tab-group__item"
+                             data-bind="
+                                click: function() {if (!disabled()) {$component.activeItem($index());}},
+                                css: {
+                                    'hrm-tab-group__item--active': $component.activeItem() === $index(),
+                                    'hrm-tab-group__item--disabled': disabled
+                                },
+                                text: text
+                             ">
+                        </div>
+                    <!-- /ko -->
+                    
+                    <!-- ko if: viewportSize().width > HRM_BREAKPOINTS.tabletMaxWidth -->
+                        <!-- ko if: maxFitItem() < items.length - 1 -->
+                            <button class="hrm-button hrm-tab-group__more-button"
+                                    data-bind="
+                                        hrmDropdownMenu,
+                                        hrmDropdownMenuTemplate: 'hrm-tab-group-more-button-dropdown-menu-template',
+                                        hrmDropdownMenuPlacement: 'bottom-end'
+                                    ">
+                                Еще...
+                            </button>
+                            
+                            <script id="hrm-tab-group-more-button-dropdown-menu-template" type="text/html">
+                                <!-- ko foreach: items.slice(maxFitItem() + 1) -->
+                                    <div class="hrm-dropdown-menu__item"
+                                         data-bind="
+                                            text: text,
+                                            click: function() {if (!disabled()) {$component.activeItem($index() + $component.maxFitItem() + 1);}},
+                                            css: {'hrm-dropdown-menu__item--disabled': disabled}
+                                         ">
+                                    </div>
+                                <!-- /ko -->
+                            </script>   
+                        <!-- /ko -->
+                    <!-- /ko -->
+                <!-- /ko -->
+            </div>
+        </div>
+    `
+});
 "use strict";
 
 ko.bindingHandlers.hrmTable = {
